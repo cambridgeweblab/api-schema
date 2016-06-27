cb02b23834a70d1009fd5fffc9bd4def53b6fe89
Sukhraj Rattan <rattan.s@cambridgeassessment.org.uk>
2016-06-27 17:00:19 +0100

    Changed the form repository to have a simple spring repo so that Optionals can be declared.

--- weblab-forms/src/main/java/ucles/weblab/common/forms/domain/FormRepository.java
+++ weblab-forms/src/main/java/ucles/weblab/common/forms/domain/FormRepository.java
@@ -1,6 +1,7 @@
 package ucles.weblab.common.forms.domain;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -9,11 +10,13 @@ import java.util.UUID;
  */
 public interface FormRepository {
     
-    <S extends Form> S save(S s);
+    <S extends FormEntity> S save(S s);
 
-    List<Form> findAllByOrderByNameAsc();
+    List<? extends FormEntity> findAllByOrderByNameAsc();
 
-    Form findOne(UUID id);
+    Optional<? extends FormEntity> findOne(UUID id);
 
-    void delete(Form form);
+    void delete(FormEntity form);
+    
+    List<? extends FormEntity> findOneByBusinessStreamAndApplicationName(String businessName, String applicationName);
 }
--- weblab-forms/src/main/java/ucles/weblab/common/forms/domain/mongo/FormRepositoryMongo.java
+++ weblab-forms/src/main/java/ucles/weblab/common/forms/domain/mongo/FormRepositoryMongo.java
@@ -1,14 +1,14 @@
 package ucles.weblab.common.forms.domain.mongo;
 
 import java.util.UUID;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import ucles.weblab.common.forms.domain.Form;
+import org.springframework.data.repository.Repository;
+import ucles.weblab.common.forms.domain.FormEntity;
 import ucles.weblab.common.forms.domain.FormRepository;
 
 /**
  *
  * @author Sukhraj
  */
-public interface FormRepositoryMongo extends FormRepository, MongoRepository<Form, UUID> {
+public interface FormRepositoryMongo extends FormRepository, Repository<FormEntity, UUID> {
     
 }
