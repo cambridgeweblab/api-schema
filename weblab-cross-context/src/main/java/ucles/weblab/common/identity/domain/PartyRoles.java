09a4fc07de872856ce6e9b4991d0d9d3adcc4d5d
Neale Upstone <neale@nealeupstone.com>
2017-06-12 12:06:14 +0100

    RES-580 add ROLE_BTS_ADMIN to the system

--- weblab-cross-context/src/main/java/ucles/weblab/common/identity/domain/PartyRoles.java
+++ weblab-cross-context/src/main/java/ucles/weblab/common/identity/domain/PartyRoles.java
@@ -9,6 +9,10 @@ public final class PartyRoles {
     public static final String ROLE_PREP_CENTRE = "ROLE_PREP_CENTRE";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
 
+    /** A role for non-technical management of the system, such as, in IELTS Results, being able to add a
+     * Recognising Organisation to the system and create a user who can administer that organisation. */
+    public static final String ROLE_BTS_ADMIN = "ROLE_BTS_ADMIN";
+
     private PartyRoles() { // Prevent instantiation
     }
 }
