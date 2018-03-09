2e8bf9933447a5b0eaeda221695ebab69299bbfc
Neale Upstone <neale@nealeupstone.com>
2018-03-09 09:32:37 +0000

    Migrate findOne() to Spring Data 2's findById() which returns Optional

--- weblab-forms/src/main/java/ucles/weblab/common/forms/domain/mongo/FormRepositoryMongo.java
+++ weblab-forms/src/main/java/ucles/weblab/common/forms/domain/mongo/FormRepositoryMongo.java
@@ -8,7 +8,5 @@ import ucles.weblab.common.forms.domain.FormRepository;
  * @author Sukhraj
  */
 public interface FormRepositoryMongo extends FormRepository, MongoRepository<FormEntityMongo, String> {
-    
-    @Override
-    FormEntityMongo findOne(String id);
+
 }
